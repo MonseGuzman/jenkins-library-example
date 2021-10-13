fedd3685f49262d8e96c8fe5e23e14be467805d9
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-12 19:11:16 -0500

    Trying to use git

--- vars/example.groovy
+++ vars/example.groovy
@@ -1,8 +1,6 @@
 def call(String name = 'User') {
-	steps {
-		echo "Install ${name}."
-		git branch: 'master',
-			credentialsId: 'global',
-			url: 'https://github.com/MonseGuzman/example-scripts.git'
-	}
+	echo "Install ${name}."
+	git branch: 'master',
+		credentialsId: 'global',
+		url: 'https://github.com/MonseGuzman/example-scripts.git'
 }
