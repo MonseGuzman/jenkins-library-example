6de36fe90f5338377ccc320d6400d87152f3dd2a
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-13 10:13:55 -0500

    Updated clone

--- vars/example.groovy
+++ vars/example.groovy
@@ -1,6 +1,8 @@
 def call(String name = 'User') {
 	echo "Install ${name}."
-	git branch: 'master',
-		credentialsId: 'global',
-		url: 'https://github.com/MonseGuzman/example-scripts.git'
+	dir("scripts"){
+		git branch: 'master',
+			credentialsId: 'global',
+			url: 'https://github.com/MonseGuzman/example-scripts.git'
+	}
 }
