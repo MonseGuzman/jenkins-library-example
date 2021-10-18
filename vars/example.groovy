478373644fce594508d2141ba8739a8cab942c3d
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-18 11:30:54 -0500

    remove tests bash

--- vars/example.groovy
+++ vars/example.groovy
@@ -4,18 +4,18 @@ def call() {
 			credentialsId: 'global',
 			url: 'https://github.com/MonseGuzman/example-scripts.git'
 	}
-	sh '''
-		ls 
-		ls scripts
-		pwd
-	'''
-	sh '''
-		chmod +x scripts/terraform-validate.sh
-		chmod +x scripts/terraform-tflint.sh
+	// sh '''
+	// 	ls 
+	// 	ls scripts
+	// 	pwd
+	// '''
+	// sh '''
+	// 	chmod +x scripts/terraform-validate.sh
+	// 	chmod +x scripts/terraform-tflint.sh
 
-		sh ./scripts/terraform-validate.sh
+	// 	sh ./scripts/terraform-validate.sh
 
-		echo "terratestTimeout - "
-		echo $terratestTimeout
-	'''
+	// 	echo "terratestTimeout - "
+	// 	echo $terratestTimeout
+	// '''
 }
