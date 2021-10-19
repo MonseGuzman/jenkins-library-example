4dba351d97d46b7e815ac0fb839f83979925c8e0
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-19 15:00:16 -0500

    test

--- vars/validate.groovy
+++ vars/validate.groovy
@@ -1,21 +1,65 @@
 def call() {
-	sh '''
-		ls 
-		echo "LS FOR SCRIPTS"
-		ls scripts
-	'''
-	sh '''
-		echo "####[command] Terraform Validate"
-		chmod +x scripts/terraform-validate.sh
+	
+	pipeline {
+		agent { docker 'kaarla/terraform-terratest' }
+		environment {
+			AWS_ACCESS_KEY_ID="${AWS_KEY_ID}"
+			AWS_SECRET_ACCESS_KEY="${AWS_SECRET_KEY}"
+			AWS_SESSION_TOKEN="${AWS_TOKEN}"
+			TFE_TOKEN="${TFE_TOKEN}"
+		}
+		stages {
+			stage('setup') {
+				steps {
+					linux 'setup'
+					
+					// example()
 
-		sh ./scripts/terraform-validate.sh
-	'''
-	sh '''
-		echo "####[command] Terraform TFLint"
-		chmod +x scripts/terraform-tflint.sh
+					sh '''
+						ls 
+						echo "LS FOR SCRIPTS"
+						ls scripts
+					'''
+				}
+			}
+			stage('validate') {
+				steps {
+					linux 'validate'
 
-		export tflint_config="scripts/tflint/.tflint.hcl"
-		echo $tflint_config
-		sh ./scripts/terraform-tflint.sh
-	'''
+					script = libraryResource 'scripts/terraform/terraform-validate'
+
+					sh script
+					
+					// sh '''
+					// 	echo "####[command] Terraform Validate"
+					// 	chmod +x scripts/terraform-validate.sh
+
+					// 	sh ./scripts/terraform-validate.sh
+					// '''
+				}
+			}
+			stage('tflint') {
+				steps {
+					linux 'tflint'
+					
+					sh '''
+						echo "####[command] Terraform TFLint"
+						chmod +x scripts/terraform-tflint.sh
+
+						export tflint_config="scripts/tflint/.tflint.hcl"
+						echo $tflint_config
+						sh ./scripts/terraform-tflint.sh
+					'''
+				}
+			}
+				
+		}
+		post {
+			always {
+				echo "Cleaning up Workspace"
+				deleteDir() /* clean up our workspace */
+			}
+		}
+	}
+
 }
