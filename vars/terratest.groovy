90b84146120544c9924b6783e6c9fa3649d1cb45
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-18 20:02:10 -0500

    Fix terratest x5

--- vars/terratest.groovy
+++ vars/terratest.groovy
@@ -1,13 +1,13 @@
 // import org.monseExample
 
 def call() {
-	TFE_WORKSPACE="terratest-${BUILD_NUMBER}"
-
 	sh '''
 		ls 
 		ls scripts
 		echo "terratestTimeout - $terratestTimeout"
-		echo TFE_WORKSPACE
+
+		export TFE_WORKSPACE = workspacesName()
+		echo $TFE_WORKSPACE
 	'''
 	// sh '''
 	// 	chmod +x scripts/create-tfe-workspace.sh
@@ -17,7 +17,9 @@ def call() {
 	sh '''
 		chmod +x scripts/terratest.sh
 		echo "Aquí deberían correr los tests"
-		echo TFE_WORKSPACE
+		
+		export TFE_WORKSPACE = workspacesName()
+		echo $TFE_WORKSPACE
 	'''
 	// echo GlobalVars.TFE_WORKSPACE
 	// sh '''
@@ -26,4 +28,8 @@ def call() {
 	// 	sh ./scripts/delete-tfe-workspace.sh
 	// '''
 	// sh ./scripts/terratest.sh
+}
+
+def workspacesName() {
+    return sh(script: 'terratest-${BUILD_NUMBER}', returnStdout: true)
 }
