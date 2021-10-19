ecfcb947276431d64f942f230e402395c9566918
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-19 18:29:05 -0500

    fix validate steps againn

--- vars/validate.groovy
+++ vars/validate.groovy
@@ -17,7 +17,7 @@ def call() {
 
 					sh '''
 						ls 
-						echo "LS FOR SCRIPTS"
+						echo "########## LS FOR SCRIPTS"
 						ls scripts
 					'''
 				}
@@ -27,18 +27,16 @@ def call() {
 					linux 'validate'
 
 					sh '''
-
-						echo "$BUILD_ID"
-						echo "$BUILD_NUMBER"
-						echo "$BUILD_TAG"
-						echo "$BUILD_URL"
-						echo "$EXECUTOR_NUMBER"
+						echo "$BUILD_ID" # 11
+						echo "$BUILD_NUMBER" # 11
+						echo "$BUILD_TAG" # jenkins-test-library-waas-11 ($BUILD_ID)
+						echo "$BUILD_URL" # http://localhost:8080/job/test-library-waas/11/
+						echo "$EXECUTOR_NUMBER" # 1
 						echo "$JAVA_HOME"
-						echo "$JENKINS_URL"
-						echo "$JOB_NAME"
-						echo "$NODE_NAME"
-						echo "$WORKSPACE"
-
+						echo "$JENKINS_URL" # http://localhost:8080/
+						echo "$JOB_NAME" # test-library-waas
+						echo "$NODE_NAME" # master
+						echo "$WORKSPACE" # /Users/monseguzman/.jenkins/workspace/test-library-waas
 					'''
 					// sh '''
 					// 	ls 
@@ -46,32 +44,26 @@ def call() {
 					// 	ls scripts
 					// '''
 					
-					// sh '''
-					// 	echo "####[command] Terraform Validate"
-					// 	chmod +x scripts/terraform-validate.sh
+					sh '''
+						echo "####[command] Terraform Validate"
+						chmod +x scripts/terraform-validate.sh
 
-					// 	sh ./scripts/terraform-validate.sh
-					// '''
+						sh ./scripts/terraform-validate.sh
+					'''
 				}
 			}
 			stage('tflint') {
 				steps {
 					linux 'tflint'
-
-					sh '''
-						ls 
-						echo "LS FOR SCRIPTS"
-						ls scripts
-					'''
 					
-					// sh '''
-					// 	echo "####[command] Terraform TFLint"
-					// 	chmod +x scripts/terraform-tflint.sh
+					sh '''
+						echo "####[command] Terraform TFLint"
+						chmod +x scripts/terraform-tflint.sh
 
-					// 	export tflint_config="scripts/tflint/.tflint.hcl"
-					// 	echo $tflint_config
-					// 	sh ./scripts/terraform-tflint.sh
-					// '''
+						export tflint_config="scripts/.tflint.hcl"
+						echo $tflint_config
+						sh ./scripts/terraform-tflint.sh
+					'''
 				}
 			}
 				
