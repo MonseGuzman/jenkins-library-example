eabe070141d5422ee036a68a7a164ba2002cccac
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-20 15:12:51 -0500

    Ops x2

--- vars/terratest.groovy
+++ vars/terratest.groovy
@@ -1,21 +1,17 @@
 def call() {
-	
-linux 'Terratest'
+	dir('test'){
+		sh '''
+			echo "####[command] Create backend"
+			
+			chmod +x scripts/override-backend.sh
+			chmod +x scripts/override-backend.sh
+		'''
 
-dir('test'){
-	sh '''
-		echo "####[command] Create backend"
-		
-		chmod +x scripts/override-backend.sh
-		chmod +x scripts/override-backend.sh
-	'''
+		sh '''
+			echo "####[command] Terratest"
 
-	sh '''
-		echo "####[command] Terratest"
-
-		chmod +x scripts/terratest.sh
-		sh ./scripts/terratest.sh
-	'''
-}
-				
+			chmod +x scripts/terratest.sh
+			sh ./scripts/terratest.sh
+		'''
+	}	
 }
--- vars/validate.groovy
+++ vars/validate.groovy
@@ -1,46 +1,45 @@
-def call() {				
-example()
+def call() {
+	sh '''
+		ls 
+		echo "########## LS FOR SCRIPTS"
+		ls scripts
+		echo "########## Mr Perfectly Fine"
+		echo "$GIT_AUTHOR"
+		echo "$SKIP_TF_VALIDATE"
+		echo "$BRANCH_NAME"
+	'''
 
-sh '''
-	ls 
-	echo "########## LS FOR SCRIPTS"
-	ls scripts
-	echo "########## Mr Perfectly Fine"
-	echo "$GIT_AUTHOR"
-	echo "$SKIP_TF_VALIDATE"
-	echo "$BRANCH_NAME"
-'''
+	linux 'validate'
 
-linux 'validate'
+	sh '''
+		echo "$GIT_AUTHOR"
+		echo "$BUILD_ID" # 11
+		echo "$BUILD_NUMBER" # 11
+		echo "$BUILD_TAG" # jenkins-test-library-waas-11 ($BUILD_ID)
+		echo "$BUILD_URL" # http://localhost:8080/job/test-library-waas/11/
+		echo "$EXECUTOR_NUMBER" # 1
+		echo "$JAVA_HOME"
+		echo "$JENKINS_URL" # http://localhost:8080/
+		echo "$JOB_NAME" # test-library-waas
+		echo "$NODE_NAME" # master
+		echo "$WORKSPACE" # /Users/monseguzman/.jenkins/workspace/test-library-waas
+	'''
 
-sh '''
-	echo "$GIT_AUTHOR"
-	echo "$BUILD_ID" # 11
-	echo "$BUILD_NUMBER" # 11
-	echo "$BUILD_TAG" # jenkins-test-library-waas-11 ($BUILD_ID)
-	echo "$BUILD_URL" # http://localhost:8080/job/test-library-waas/11/
-	echo "$EXECUTOR_NUMBER" # 1
-	echo "$JAVA_HOME"
-	echo "$JENKINS_URL" # http://localhost:8080/
-	echo "$JOB_NAME" # test-library-waas
-	echo "$NODE_NAME" # master
-	echo "$WORKSPACE" # /Users/monseguzman/.jenkins/workspace/test-library-waas
-'''
+	sh '''
+		echo "####[command] Terraform Validate"
+		chmod +x scripts/terraform-validate.sh
 
-sh '''
-	echo "####[command] Terraform Validate"
-	chmod +x scripts/terraform-validate.sh
+		sh ./scripts/terraform-validate.sh
+	'''
 
-	sh ./scripts/terraform-validate.sh
-'''
+	linux 'tflint'
 
-linux 'tflint'
+	sh '''
+		echo "####[command] Terraform TFLint"
+		chmod +x scripts/terraform-tflint.sh
 
-sh '''
-	echo "####[command] Terraform TFLint"
-	chmod +x scripts/terraform-tflint.sh
-
-	export tflint_config="scripts/.tflint.hcl"
-	echo $tflint_config
-	sh ./scripts/terraform-tflint.sh
-'''
+		export tflint_config="scripts/.tflint.hcl"
+		echo $tflint_config
+		sh ./scripts/terraform-tflint.sh
+	'''
+}
