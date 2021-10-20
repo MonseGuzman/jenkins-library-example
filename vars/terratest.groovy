d4ec80ab2a3afd92841436fb446c5c5ddeb0f3e5
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-20 15:30:01 -0500

    Fix folder

--- vars/terratest.groovy
+++ vars/terratest.groovy
@@ -1,17 +1,26 @@
 def call() {
+	sh '''
+		echo "########## LS command"
+		ls 
+		echo "########## Pay my rent"
+		echo "$GIT_AUTHOR"
+		echo "$terratestTimeout"
+		echo "$BRANCH_NAME"
+	'''
+
 	dir('test'){
 		sh '''
 			echo "####[command] Create backend"
 			
-			chmod +x scripts/override-backend.sh
-			chmod +x scripts/override-backend.sh
+			chmod +x override-backend.sh
+			sh ./override-backend.sh
 		'''
 
 		sh '''
 			echo "####[command] Terratest"
 
-			chmod +x scripts/terratest.sh
-			sh ./scripts/terratest.sh
+			chmod +x terratest.sh
+			sh ./terratest.sh
 		'''
 	}	
 }
