d57fb01f08827e76e598e0040b54a21e71161f10
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-20 15:06:28 -0500

    Fix scripts? maybe

---
+++ vars/ssm-pipeline.groovy
@@ -0,0 +1,47 @@
+def call() {
+	
+	pipeline {
+		agent { docker 'kaarla/terraform-terratest' }
+		environment {
+			AWS_ACCESS_KEY_ID="${AWS_KEY_ID}"
+			AWS_SECRET_ACCESS_KEY="${AWS_SECRET_KEY}"
+			AWS_SESSION_TOKEN="${AWS_TOKEN}"
+			TFE_TOKEN="${TFE_TOKEN}"
+		}
+		stages {
+			stage('setup') {
+				steps {
+					linux 'setup'
+
+					script{
+						env.GIT_AUTHOR = sh(script: "eval git --no-pager show -s --format=\'%an\'", returnStdout: true).trim()
+					}
+					
+					example()
+				}
+			}
+			stage('validate') {
+				steps {
+					linux 'validate'
+
+					validate()
+				}
+			}
+			stage('terratest') {
+				steps {
+					linux 'terratest'
+					
+					terratest()
+				}
+			}
+				
+		}
+		post {
+			always {
+				echo "Cleaning up Workspace"
+				deleteDir() /* clean up our workspace */
+			}
+		}
+	}
+
+}
--- vars/terratest.groovy
+++ vars/terratest.groovy
@@ -1,48 +1,21 @@
 def call() {
 	
-stages {
-	stage('setup') {
-		steps {
-			linux 'setup'
+linux 'Terratest'
 
-			script{
-				env.TFE_WORKSPACE = sh(script: "eval terratest-${BUILD_NUMBER}", returnStdout: true).trim()
-			}
-			
-			example()
-
-			sh '''
-				ls 
-				echo "########## LS FOR SCRIPTS"
-				ls scripts
-				echo "########## Shout Out to my ex"
-				echo "$TFE_WORKSPACE"
-				echo "$terratestTimeout"
-				echo "$BRANCH_NAME"
-			'''
-		}
-	}
-	stage('Terratest') {
-		steps {
-			linux 'Terratest'
-			
-			dir('test'){
-				sh '''
-					echo "####[command] Create backend"
-					
-					chmod +x scripts/override-backend.sh
-					chmod +x scripts/override-backend.sh
-
-				'''
-				sh '''
-					echo "####[command] Terratest"
-
-					chmod +x scripts/terratest.sh
-					sh ./scripts/terratest.sh
-				'''
-			}
-
-		}
-	}
+dir('test'){
+	sh '''
+		echo "####[command] Create backend"
 		
+		chmod +x scripts/override-backend.sh
+		chmod +x scripts/override-backend.sh
+	'''
+
+	sh '''
+		echo "####[command] Terratest"
+
+		chmod +x scripts/terratest.sh
+		sh ./scripts/terratest.sh
+	'''
+}
+				
 }
