1410395857cb12d9b6f4bd8248f21431729d3fef
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-20 14:55:40 -0500

    remove pipeline block

--- vars/terratest.groovy
+++ vars/terratest.groovy
@@ -1,63 +1,48 @@
 def call() {
-	pipeline {
-		agent { docker 'kaarla/terraform-terratest' }
-		environment {
-			AWS_ACCESS_KEY_ID="${AWS_KEY_ID}"
-			AWS_SECRET_ACCESS_KEY="${AWS_SECRET_KEY}"
-			AWS_SESSION_TOKEN="${AWS_TOKEN}"
-			TFE_TOKEN="${TFE_TOKEN}"
-		}
-		stages {
-			stage('setup') {
-				steps {
-					linux 'setup'
+	
+stages {
+	stage('setup') {
+		steps {
+			linux 'setup'
 
-					script{
-						env.TFE_WORKSPACE = sh(script: "eval terratest-${BUILD_NUMBER}", returnStdout: true).trim()
-					}
-					
-					example()
-
-					sh '''
-						ls 
-						echo "########## LS FOR SCRIPTS"
-						ls scripts
-						echo "########## Shout Out to my ex"
-						echo "$TFE_WORKSPACE"
-						echo "$terratestTimeout"
-						echo "$BRANCH_NAME"
-					'''
-				}
+			script{
+				env.TFE_WORKSPACE = sh(script: "eval terratest-${BUILD_NUMBER}", returnStdout: true).trim()
 			}
-			stage('Terratest') {
-				steps {
-					linux 'Terratest'
-					
-					dir('test'){
-						sh '''
-							echo "####[command] Create backend"
-							
-							chmod +x scripts/override-backend.sh
-							chmod +x scripts/override-backend.sh
+			
+			example()
 
-						'''
-						sh '''
-							echo "####[command] Terratest"
-
-							chmod +x scripts/terratest.sh
-							sh ./scripts/terratest.sh
-						'''
-					}
-
-				}
-			}
-				
-		}
-		post {
-			always {
-				echo "Cleaning up Workspace"
-				deleteDir() /* clean up our workspace */
-			}
+			sh '''
+				ls 
+				echo "########## LS FOR SCRIPTS"
+				ls scripts
+				echo "########## Shout Out to my ex"
+				echo "$TFE_WORKSPACE"
+				echo "$terratestTimeout"
+				echo "$BRANCH_NAME"
+			'''
 		}
 	}
+	stage('Terratest') {
+		steps {
+			linux 'Terratest'
+			
+			dir('test'){
+				sh '''
+					echo "####[command] Create backend"
+					
+					chmod +x scripts/override-backend.sh
+					chmod +x scripts/override-backend.sh
+
+				'''
+				sh '''
+					echo "####[command] Terratest"
+
+					chmod +x scripts/terratest.sh
+					sh ./scripts/terratest.sh
+				'''
+			}
+
+		}
+	}
+		
 }
--- vars/validate.groovy
+++ vars/validate.groovy
@@ -1,88 +1,71 @@
 def call() {
-	
-	pipeline {
-		agent { docker 'kaarla/terraform-terratest' }
-		environment {
-			AWS_ACCESS_KEY_ID="${AWS_KEY_ID}"
-			AWS_SECRET_ACCESS_KEY="${AWS_SECRET_KEY}"
-			AWS_SESSION_TOKEN="${AWS_TOKEN}"
-			TFE_TOKEN="${TFE_TOKEN}"
-		}
-		stages {
-			stage('setup') {
-				steps {
-					linux 'setup'
 
-					script{
-						env.GIT_AUTHOR = sh(script: "eval git --no-pager show -s --format=\'%an\'", returnStdout: true).trim()
-					}
-					
-					example()
+stages {
+	stage('setup') {
+		steps {
+			linux 'setup'
 
-					sh '''
-						ls 
-						echo "########## LS FOR SCRIPTS"
-						ls scripts
-						echo "########## Mr Perfectly Fine"
-						echo "$GIT_AUTHOR"
-						echo "$SKIP_TF_VALIDATE"
-						echo "$BRANCH_NAME"
-					'''
-				}
+			script{
+				env.GIT_AUTHOR = sh(script: "eval git --no-pager show -s --format=\'%an\'", returnStdout: true).trim()
 			}
-			stage('validate') {
-				steps {
-					linux 'validate'
+			
+			example()
 
-					sh '''
-						echo "$GIT_AUTHOR"
-						echo "$BUILD_ID" # 11
-						echo "$BUILD_NUMBER" # 11
-						echo "$BUILD_TAG" # jenkins-test-library-waas-11 ($BUILD_ID)
-						echo "$BUILD_URL" # http://localhost:8080/job/test-library-waas/11/
-						echo "$EXECUTOR_NUMBER" # 1
-						echo "$JAVA_HOME"
-						echo "$JENKINS_URL" # http://localhost:8080/
-						echo "$JOB_NAME" # test-library-waas
-						echo "$NODE_NAME" # master
-						echo "$WORKSPACE" # /Users/monseguzman/.jenkins/workspace/test-library-waas
-					'''
-					// sh '''
-					// 	ls 
-					// 	echo "LS FOR SCRIPTS"
-					// 	ls scripts
-					// '''
-					
-					sh '''
-						echo "####[command] Terraform Validate"
-						chmod +x scripts/terraform-validate.sh
-
-						sh ./scripts/terraform-validate.sh
-					'''
-				}
-			}
-			stage('tflint') {
-				steps {
-					linux 'tflint'
-					
-					sh '''
-						echo "####[command] Terraform TFLint"
-						chmod +x scripts/terraform-tflint.sh
-
-						export tflint_config="scripts/.tflint.hcl"
-						echo $tflint_config
-						sh ./scripts/terraform-tflint.sh
-					'''
-				}
-			}
-				
-		}
-		post {
-			always {
-				echo "Cleaning up Workspace"
-				deleteDir() /* clean up our workspace */
-			}
+			sh '''
+				ls 
+				echo "########## LS FOR SCRIPTS"
+				ls scripts
+				echo "########## Mr Perfectly Fine"
+				echo "$GIT_AUTHOR"
+				echo "$SKIP_TF_VALIDATE"
+				echo "$BRANCH_NAME"
+			'''
 		}
 	}
+	stage('validate') {
+		steps {
+			linux 'validate'
 
+			sh '''
+				echo "$GIT_AUTHOR"
+				echo "$BUILD_ID" # 11
+				echo "$BUILD_NUMBER" # 11
+				echo "$BUILD_TAG" # jenkins-test-library-waas-11 ($BUILD_ID)
+				echo "$BUILD_URL" # http://localhost:8080/job/test-library-waas/11/
+				echo "$EXECUTOR_NUMBER" # 1
+				echo "$JAVA_HOME"
+				echo "$JENKINS_URL" # http://localhost:8080/
+				echo "$JOB_NAME" # test-library-waas
+				echo "$NODE_NAME" # master
+				echo "$WORKSPACE" # /Users/monseguzman/.jenkins/workspace/test-library-waas
+			'''
+			// sh '''
+			// 	ls 
+			// 	echo "LS FOR SCRIPTS"
+			// 	ls scripts
+			// '''
+			
+			sh '''
+				echo "####[command] Terraform Validate"
+				chmod +x scripts/terraform-validate.sh
+
+				sh ./scripts/terraform-validate.sh
+			'''
+		}
+	}
+	stage('tflint') {
+		steps {
+			linux 'tflint'
+			
+			sh '''
+				echo "####[command] Terraform TFLint"
+				chmod +x scripts/terraform-tflint.sh
+
+				export tflint_config="scripts/.tflint.hcl"
+				echo $tflint_config
+				sh ./scripts/terraform-tflint.sh
+			'''
+		}
+	}
+		
 }
