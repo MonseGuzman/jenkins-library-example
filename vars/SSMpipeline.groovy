aeb3b9cf9132d0f329e966cc1600a6b551c50049
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-25 15:45:21 -0500

    fix branch name x4

--- vars/SSMpipeline.groovy
+++ vars/SSMpipeline.groovy
@@ -32,18 +32,17 @@ def call() {
 					linux 'terratest'
 
 					script{
-						def BRANCH = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+						// def BRANCH = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
 						env.GIT_REPO_NAME = env.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
 						env.TFE_WORKSPACE = sh(script: "eval echo 'terratest-$BUILD_ID-$GIT_REPO_NAME'", returnStdout: true).trim()
 						env.BRANCH_NAME = sh(script: "eval git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
+						env.BRANCH_NAME_U = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
 					}
 
 					sh 'echo $GIT_REPO_NAME'
 					sh 'echo $TFE_WORKSPACE'
 					sh 'echo $BRANCH_NAME'
-					sh 'echo "EXAMPLE"'
-					sh 'echo ${BRANCH}'
-					sh 'echo ${env.BRANCH_NAME}'
+					sh 'echo $BRANCH_NAME_U'
 					// terratest()
 				}
 			}
