acdf26178c04d975d8b7cd6adf9bc7cdd39140a6
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-28 15:23:09 -0500

    added set -e

--- resources/com/monseExample/scripts/terraform/export.sh
+++ resources/com/monseExample/scripts/terraform/export.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 echo "##[group] dwarf.config logic"
 echo "##[debug] ENV_NAME: $ENV_NAME"
 
 echo "##[debug] Reading dwarf.config file and exporting ENV variables"
 echo $(grep -v '^#' dwarf.config | xargs) 
 echo "##[debug] Declared variables are:"
-array=( `awk -F '=' '{ print $1 }' dwarf.config` )
-array2=( `awk -F '=' '{ print $2 }' dwarf.config`)
+array=($(awk -F '=' '{ print $1 }' dwarf.config))
+array2=($(awk -F '=' '{ print $2 }' dwarf.config))
 i=0
 while [ $i -lt ${#array[*]} ]; do
     if [ "${array[$i]}" == "LANDING_ZONE" ]; then
