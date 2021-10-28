e9c4f8aeba7b23d6d867e147569975d83b2f54cb
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-10-28 14:43:14 -0500

    peace

--- vars/loadDwarfconfig.groovy
+++ vars/loadDwarfconfig.groovy
@@ -1,15 +1,30 @@
-def loadEnvironmentVariables(){
-    def props = readProperties file: 'dwarf.config'
-
-    keys=props.keySet()
-    for(key in keys){
-        value = props["${key}"]
-        env."${key}" = "${value}"
-    }
-}
-
 def call(){
-    loadEnvironmentVariables()
+    sh '''
+    echo "##[group] dwarf.config logic"
+    echo "##[debug] ENV_NAME: $ENV_NAME"
+
+    echo "##[debug] Reading dwarf.config file and exporting ENV variables"
+    echo $(grep -v '^#' dwarf.config | xargs) 
+    echo "##[debug] Declared variables are:"
+    array=($(awk -F '=' '{ print $1 }' dwarf.config))
+    array2=($(awk -F '=' '{ print $2 }' dwarf.config))
+    i=0
+    while [ $i -lt ${#array[*]} ]; do
+        if [ "${array[$i]}" == "LANDING_ZONE" ]; then
+            LANDING_ZONE=${array2[$i]}
+        fi
+
+        if [ "${array[$i]}" == "SUPPORTED_PRODUCT_WORKLOAD" ]; then
+            SUPPORTED_PRODUCT_WORKLOAD=${array2[$i]}
+        fi
+
+        echo "##[debug] setting ${array[$i]} = ${array2[$i]}"
+        echo "##vso[task.setvariable variable=${array[$i]};isOutput=true]${array2[$i]}"
+        i=$(( $i + 1));
+    done
+
+    echo "##[endgroup]"
+    '''
 
     sh 'printenv'
 }
