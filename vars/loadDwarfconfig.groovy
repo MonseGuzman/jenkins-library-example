aa338a0c9a41bcdad480c1ec139c5a0662236725
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-11-01 13:05:05 -0600

    print env

--- vars/loadDwarfconfig.groovy
+++ vars/loadDwarfconfig.groovy
@@ -1,4 +1,4 @@
-def env = System.getenv()
+// def env = System.getenv()
 
 def call(){
     sh '''
@@ -6,7 +6,7 @@ def call(){
 
         source scripts/export.sh
 
-        echo env['TERRAFORM_DESTROY']
+        echo env.TERRAFORM_DESTROY=$TERRAFORM_DESTROY
     '''
 
     // withEnv(["TERRAFORM_DESTROY=newbar"]) {
