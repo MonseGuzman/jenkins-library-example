43f0bd5adcc6c2674efc6d02103a67821daa217b
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-11-01 17:49:55 -0600

    change to trueee

--- vars/SSMpipeline.groovy
+++ vars/SSMpipeline.groovy
@@ -7,7 +7,7 @@ def call() {
 			AWS_SECRET_ACCESS_KEY="${AWS_SECRET_KEY}"
 			AWS_SESSION_TOKEN="${AWS_TOKEN}"
 			TFE_TOKEN="${TFE_TOKEN}"
-			TERRAFORM_DESTROY="FALSE"
+			TERRAFORM_DESTROY="TRUE"
 		}
 		stages {
 			stage('setup') {
