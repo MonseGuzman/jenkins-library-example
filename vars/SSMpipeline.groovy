9c849dfcb3126a5e93f006cabe11c02487c5bdda
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-11-02 13:06:32 -0600

    test - env variable

--- vars/SSMpipeline.groovy
+++ vars/SSMpipeline.groovy
@@ -42,10 +42,10 @@ def call() {
 				steps {
 					loadDwarfconfig()
 					script {
-						env.TF_DESTROY = sh'''
+						env.TF_DESTROY = sh returnStdout: true, script: """
 							source scripts/export.sh
         					eval echo '$TERRAFORM_DESTROY'
-						'''
+						"""
 					}
 
 					sh 'echo ${env.TF_DESTROY}'
