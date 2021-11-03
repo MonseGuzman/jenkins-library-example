144fdcd5425e0c68d9e00054de1828fcabb61ade
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-11-03 13:56:12 -0600

    :thinking: ahhh

--- vars/SSMpipeline.groovy
+++ vars/SSMpipeline.groovy
@@ -41,10 +41,12 @@ def call() {
 			stage('export dwarf vars'){
 				steps {
 					script{
-						env.TF_DESTROY=sh(script: "eval sh ./scripts/export.sh", returnStdout: true).trim()
+						env.TF_DESTROY=sh(script: "eval echo `./export.sh`", returnStdout: true).trim()
 					}
 
 					// loadDwarfconfig()
+
+					sh 'echo $TF_DESTROY'
 					
 					// script {
 					// 	def test = sh(script: '''
