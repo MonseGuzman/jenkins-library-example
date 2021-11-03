6b7ad8e87a186b6a5076ba5a4b2b461989a22974
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-11-03 10:50:40 -0600

    added call

--- vars/terraformDestroy.groovy
+++ vars/terraformDestroy.groovy
@@ -1,9 +1,11 @@
-sh '''
-    ls 
-    echo "####[command] Terraform destroy"
-    
-    chmod +x scripts/export.sh scripts/terraform-destroy.sh
+def call(){
+    sh '''
+        ls 
+        echo "####[command] Terraform destroy"
+        
+        chmod +x scripts/export.sh scripts/terraform-destroy.sh
 
-    source scripts/export.sh
-    sh ./scripts/terraform-destroy.sh
-'''
+        source scripts/export.sh
+        sh ./scripts/terraform-destroy.sh
+    '''
+}
