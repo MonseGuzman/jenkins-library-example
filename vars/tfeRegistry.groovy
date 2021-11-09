83170e66f61de0f00f0345f0e5a1ce9fc9de6a87
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-11-09 17:45:57 -0600

    test - 1.4

--- vars/tfeRegistry.groovy
+++ vars/tfeRegistry.groovy
@@ -1,19 +1,22 @@
+def tfeToken
+
 def call() {
 	def MY_PASSWORD = "YWVyY3dxZWY"
 
-	def test = sh (
-		script: "eval echo 'hello'",
-		returnStdout: true
-	).trim()
+	tfeToken = sh(
+		script: 'echo `cat .terraformrc | grep "token" | awk \'{printf $2}\' | tr -d \' " \' `',
+		returnStdout: true,
+  	)
 
-	sh 'echo ${test}'
-	sh 'echo $test'
+	sh 'echo ${tfeToken}'
+	sh 'echo $tfeToken'
 
-	wrap([$class: "MaskPasswordsBuildWrapper",
-			varPasswordPairs: [[password: MY_PASSWORD], [password: test]]]) {
-		echo "Password: ${MY_PASSWORD}"
-		echo "test: ${test}"
-	}
+	// wrap([$class: "MaskPasswordsBuildWrapper",
+	// 		varPasswordPairs: [[password: MY_PASSWORD], [password: test]]]) {
+	// 	echo "Password: ${MY_PASSWORD}"
+	// 	echo "test: ${test}"
+	// }
+
 	// sh '''
 	// 	chmod +x scripts/tfe-private-module.sh
 	// 	sh ./scripts/tfe-private-module.sh
