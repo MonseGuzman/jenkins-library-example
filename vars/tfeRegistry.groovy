817d9261043f2e798f8fa344185d0e91f3d81842
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-11-10 15:22:52 -0600

    almost - we were happy - taylor swift (TV)

--- vars/tfeRegistry.groovy
+++ vars/tfeRegistry.groovy
@@ -1,31 +1,25 @@
-// def tfeToken
-
 def call() {
 	def MY_PASSWORD = '`cat .terraformrc | grep "token" | awk \'{printf $2}\' | tr -d \' " \' `'
 
-	tfeToken = sh(
-		script: 'eval echo `cat .terraformrc | grep "token" | awk \'{printf $2}\' | tr -d \' " \' `',
-		returnStdout: true,
-	).trim()
+	// tfeToken = sh(
+	// 	script: 'eval echo `cat .terraformrc | grep "token" | awk \'{printf $2}\' | tr -d \' " \' `',
+	// 	returnStdout: true,
+	// ).trim()
 
 	sh 'echo "ay noooo"'
 
 	sh """
-		echo ${tfeToken}
 		echo 'esta es la variable MY_PASSWORD'
 		echo ${MY_PASSWORD}
 		echo 'end sh'
 	"""
 
 	wrap([$class: "MaskPasswordsBuildWrapper",
-			varPasswordPairs: [[password: MY_PASSWORD], [password: tfeToken]]]) {
-		echo "Password: ${MY_PASSWORD}"
-		echo "test: ${tfeToken}"
+			varPasswordPairs: [[password: MY_PASSWORD]]]) {
+		echo "TFE_TOKEN: ${MY_PASSWORD}"
 
 		sh """
 			set +x
-			chmod +x scripts/tfe-private-module.sh
-			sh ./scripts/tfe-private-module.sh ${tfeToken}
 			echo 'esta es la variable MY_PASSWORD'
 			sh ./scripts/tfe-private-module.sh ${MY_PASSWORD}
 		"""
