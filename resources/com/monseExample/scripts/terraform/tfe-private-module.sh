02cb7d64504f79128a74a8427febd7d9256c2899
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-11-16 10:47:43 -0600

    Added all the script to show in the demo picture

--- resources/com/monseExample/scripts/terraform/tfe-private-module.sh
+++ resources/com/monseExample/scripts/terraform/tfe-private-module.sh
@@ -28,46 +28,46 @@ echo "##[debug]The module name: $code"
 #   echo "lloro"
 # fi
 
-# if [ "$moduleName" == "$code" ]; then
-#   echo "##[debug]The $CIRCLE_PROJECT_REPONAME is already uploaded into $TFE_ORG"
-# else
-#   echo "##[debug]Creating PAYLOAD"
-#   PAYLOAD=$(cat <<EOF
-#   {
-#     "data": {
-#       "attributes": {
-#         "vcs-repo": {
-#             "identifier":"$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME",
-#             "oauth-token-id":"$OAUTH_TOKEN_ID",
-#             "display_identifier":"$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
-#           }
-#       },
-#       "type":"registry-modules"
-#     }
-#   }
-# EOF
-# )
+if [ "$moduleName" == "$code" ]; then
+  echo "##[debug]The $CIRCLE_PROJECT_REPONAME is already uploaded into $TFE_ORG"
+else
+  echo "##[debug]Creating PAYLOAD"
+  PAYLOAD=$(cat <<EOF
+  {
+    "data": {
+      "attributes": {
+        "vcs-repo": {
+            "identifier":"$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME",
+            "oauth-token-id":"$OAUTH_TOKEN_ID",
+            "display_identifier":"$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
+          }
+      },
+      "type":"registry-modules"
+    }
+  }
+EOF
+)
 
-#   echo "##[debug]PAYLOAD"
-#   echo $PAYLOAD
-#   echo "##[debug]Publishing the '$CIRCLE_PROJECT_REPONAME' SSM on $TFE_ORG"
+  echo "##[debug]PAYLOAD"
+  echo $PAYLOAD
+  echo "##[debug]Publishing the '$CIRCLE_PROJECT_REPONAME' SSM on $TFE_ORG"
 
-#   status=$(curl \
-#     --silent \
-#     --output /dev/null \
-#     --write-out '%{http_code}' \
-#     --header "Authorization: Bearer ${tfeToken}" \
-#     --header "Content-Type: application/vnd.api+json" \
-#     --request POST \
-#     --data "$PAYLOAD" \
-#     https://$TFE_HOST/api/v2/organizations/$TFE_ORG/registry-modules/vcs)
+  status=$(curl \
+    --silent \
+    --output /dev/null \
+    --write-out '%{http_code}' \
+    --header "Authorization: Bearer ${MY_PASSWORD}" \
+    --header "Content-Type: application/vnd.api+json" \
+    --request POST \
+    --data "$PAYLOAD" \
+    https://$TFE_HOST/api/v2/organizations/$TFE_ORG/registry-modules/vcs)
 
-#     echo $status
+    echo $status
   
-#   if [ "$status" -eq 201 ]; then
-#     echo "##[debug]Successfully published module version"
-#   else
-#     echo "##[error]An error has occured while publishing the SSM. Response code $status"
-#     exit 1
-#   fi
-# fi
+  if [ "$status" -eq 201 ]; then
+    echo "##[debug]Successfully published module version"
+  else
+    echo "##[error]An error has occured while publishing the SSM. Response code $status"
+    exit 1
+  fi
+fi
