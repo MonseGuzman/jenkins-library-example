530165a2141ec5e1468fbd887185b2f165ceba77
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2021-12-15 17:43:32 -0600

    change if

--- vars/loadDwarfconfig.groovy
+++ vars/loadDwarfconfig.groovy
@@ -3,7 +3,7 @@ def call(){
         env.LATEST_VERSION = sh(script: "eval git tag | sort -V | tail -1", returnStdout: true).trim()
     }
 
-    if (env.LATEST_VERSION != null){
+    if (env.LATEST_VERSION != ""){
         sh "echo 'inside of ---if--- ${LATEST_VERSION}'"
     }
 
