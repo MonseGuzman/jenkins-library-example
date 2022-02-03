dcdfb166141543739bdb6dd48ce198126c354b9b
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-02-02 18:02:22 -0600

    umm idk

--- vars/destroy.groovy
+++ vars/destroy.groovy
@@ -1,13 +1,13 @@
 def call(String execution = 'none'){
     def MY_PASSWORD = '`cat .terraformrc | grep "token" | awk \'{printf $2}\' | tr -d \' " \' `'
 
-    if (${execution} == 'create') {
+    if (execution == 'create') {
         sh """
             set +x
             echo "create"
 			echo ${MY_PASSWORD}
         """
-    } else if (${execution} == 'delete') {
+    } else if (execution == 'delete') {
         // currentBuild.result = 'FAILED'
         // return
          sh """
