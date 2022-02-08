7bdd36a83a30bf5e1996ec20a4d48185e4482f17
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-02-08 11:38:20 -0600

    testing here :(

--- vars/SSMpipeline.groovy
+++ vars/SSMpipeline.groovy
@@ -41,8 +41,11 @@ def call() {
 					sh """
 						echo "flag: ${SKIP_TF_VALIDATE}"
 					"""
+					script{
+						env.MAX_RUN_TIME = ${currentBuild.durationString.replace(' and counting', '')}
+					}
 					sh """
-						echo "${currentBuild.durationString}"
+						echo "${MAX_RUN_TIME}"
 					"""
 				}
 			}
