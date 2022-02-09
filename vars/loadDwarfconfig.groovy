fe0567938973147a1fb8a62064be4cbf8e2e801d
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-02-08 18:39:30 -0600

    ñañaña

--- vars/loadDwarfconfig.groovy
+++ vars/loadDwarfconfig.groovy
@@ -1,6 +1,7 @@
 def call(){
     script{
         env.LATEST_VERSION = sh(script: "eval git tag | sort -V | tail -1", returnStdout: true).trim()
+        env.MAX_RUN_TIME = "${currentBuild.durationString.replace(' y contando', '')}"
     }
 
     if (env.LATEST_VERSION != ""){
@@ -9,7 +10,7 @@ def call(){
 
     sh "echo 'outside of --if--' "
 
-    def MAX_RUN_TIME = "${currentBuild.durationString.replace(' y contando', '')}"
+    // def MAX_RUN_TIME = "${currentBuild.durationString.replace(' y contando', '')}"
 
     sh """
         echo "${MAX_RUN_TIME}"
