c4d8eb232c44a1572dcc0056292eb1638c47fc13
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-03-03 17:11:23 -0600

    ñañaña (

--- resources/com/monseExample/scripts/terraform/terraform-destroy.sh
+++ resources/com/monseExample/scripts/terraform/terraform-destroy.sh
@@ -5,6 +5,35 @@ export TFE_HOST="app.terraform.io"
 export TFE_TOKEN="$1"
 export TFE_WORKSPACE="terratest-2"
 
+set -e
+
+function add_tfe_workspace_var() {
+    echo "##[debug]Retrieving variables..."
+
+    TF_VARIABLES=$(curl \
+        --silent \
+        --header "Authorization: Bearer $TFE_TOKEN" \
+        --header "Content-Type: application/vnd.api+json" \
+        --request GET \
+        https://$TFE_HOST/api/v2/workspaces/$WORKSPACE_ID/vars | jq -r ".data[].attributes")
+
+    echo $TF_VARIABLES
+    echo "####################"
+
+    VAR_KEY=(`echo $TF_VARIABLES | jq -r ".key"`)
+    VAR_VALUE=(`echo $TF_VARIABLES | jq -r ".value"`)
+
+    i=0
+
+    while [ $i -lt ${#VAR_KEY[*]} ]; do
+        echo "##[debug] setting ${VAR_KEY[$i]} = ${VAR_VALUE[$i]}"
+        export ${VAR_KEY[$i]}=${VAR_VALUE[$i]}
+        
+        i=$(( $i + 1));
+    done
+}
+
+echo "##[debug]Retrieving workspace ID..."
 export WORKSPACE_ID=$(curl \
     --silent \
     --header "Authorization: Bearer $TFE_TOKEN" \
@@ -13,26 +42,4 @@ export WORKSPACE_ID=$(curl \
     https://$TFE_HOST/api/v2/organizations/$TFE_ORG/workspaces/$TFE_WORKSPACE | jq -r ".data.id")
 echo "##[debug]Workspace ID is $WORKSPACE_ID"
 
-echo "##[debug]Retrieving variables..."
-
-TF_VARIABLES=$(curl \
-    --silent \
-    --header "Authorization: Bearer $TFE_TOKEN" \
-    --header "Content-Type: application/vnd.api+json" \
-    --request GET \
-    https://$TFE_HOST/api/v2/workspaces/$WORKSPACE_ID/vars | jq -r ".data[].attributes")
-
-echo $TF_VARIABLES
-echo "####################"
-
-VAR_KEY=(`echo $TF_VARIABLES | jq -r ".key"`)
-VAR_VALUE=(`echo $TF_VARIABLES | jq -r ".value"`)
-
-i=0
-
-while [ $i -lt ${#VAR_KEY[*]} ]; do
-    echo "##[debug] setting ${VAR_KEY[$i]} = ${VAR_VALUE[$i]}"
-    export ${VAR_KEY[$i]}=${VAR_VALUE[$i]}
-    
-    i=$(( $i + 1));
-done
+add_tfe_workspace_var
