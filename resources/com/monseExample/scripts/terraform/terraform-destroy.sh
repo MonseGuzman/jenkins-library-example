d743b76b2ff823cec6787df92e2c076d1318c3c2
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-03-03 16:46:42 -0600

    opsiiii

--- resources/com/monseExample/scripts/terraform/terraform-destroy.sh
+++ resources/com/monseExample/scripts/terraform/terraform-destroy.sh
@@ -5,6 +5,14 @@ export TFE_HOST="app.terraform.io"
 export TFE_TOKEN="$1"
 export TFE_WORKSPACE="terratest-2"
 
+export WORKSPACE_ID=$(curl \
+    --silent \
+    --header "Authorization: Bearer $TFE_TOKEN" \
+    --header "Content-Type: application/vnd.api+json" \
+    --request GET \
+    https://$TFE_HOST/api/v2/organizations/$TFE_ORG/workspaces/$TFE_WORKSPACE | jq -r ".data.id")
+echo "##[debug]Workspace ID is $WORKSPACE_ID"
+
 echo "##[debug]Retrieving variables..."
 
 TF_VARIABLES=$(curl \
