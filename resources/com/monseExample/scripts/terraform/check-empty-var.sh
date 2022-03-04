361d2d823eb1dfd74b7f24d8cdec61e9cd6778e3
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-03-04 10:23:34 -0600

    echo variables

--- resources/com/monseExample/scripts/terraform/check-empty-var.sh
+++ resources/com/monseExample/scripts/terraform/check-empty-var.sh
@@ -2,7 +2,10 @@
 
 if [ -n "$1" ]; then
     echo "im not empty :("
-    echo $1
+    echo $ARM_TENANT_ID
+    echo $ARM_SUBSCRIPTION_ID
+    echo $ARM_CLIENT_SECRET
+    echo $ARM_CLIENT_ID
 else 
     echo "im empty :)"
 fi
