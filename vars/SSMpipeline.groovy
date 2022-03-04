c1eeff71bab2ca58c544a597c5fcaa44b5259493
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-03-04 11:24:21 -0600

    ay no

--- vars/SSMpipeline.groovy
+++ vars/SSMpipeline.groovy
@@ -84,9 +84,11 @@ def call() {
 			stage('destroy'){
 				steps {
 					sh """
-						chmod +x scripts/terraform-destroy.sh
+						chmod +x scripts/terraform-destroy.sh scripts/check-empty-var.sh
 						source scripts/terraform-destroy.sh ${TFE_TOKEN}
-						echo $ARM_TENANT_ID
+						echo "$ARM_TENANT_ID"
+
+						sh ./scripts/check-empty-var.sh "ARM_TENANT_ID"
 					"""
 				}
 			}
