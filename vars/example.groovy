16749e5634590f1fdd5eb4a7f3a50253a0b64cb4
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-03-18 15:20:42 -0600

    mmm...

--- vars/example.groovy
+++ vars/example.groovy
@@ -1,15 +1,14 @@
 def call() {
 	sh 'printenv'
 
-	def SHARED_LIBRARY_VER = env."library.library-example.version"
+	// def SHARED_LIBRARY_VER = env."library.library-example.version"
 
 	sh """
-		echo ${SHARED_LIBRARY_VER}
 		echo ${library.library-example.version}
 	"""
 	
 	dir("resources"){
-		git branch: 'SHARED_LIBRARY_VER',
+		git branch: "${library.library-example.version}",
 			credentialsId: 'global',
 			url: 'https://github.com/MonseGuzman/jenkins-library-example.git'
 	}
