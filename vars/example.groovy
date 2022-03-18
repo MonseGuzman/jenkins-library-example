9178b5fe3c81cf506089c38c14f8b96e0f1c982d
Monserrat Guzman <monserrat.guzman@digitalonus.com>
2022-03-18 15:11:00 -0600

    idk how to packer use :/

--- vars/example.groovy
+++ vars/example.groovy
@@ -1,16 +1,13 @@
 def call() {
 	sh 'printenv'
 
-	def SHARED_LIBRARY_VER = env."library.library-example.version"
+	// def SHARED_LIBRARY_VER = env."library.library-example.version"
 
 	sh '''
 		echo ${SHARED_LIBRARY_VER}
+		echo ${library.library-example.version}
 	'''
-	// dir("scripts"){
-	// 	git branch: 'master',
-	// 		credentialsId: 'global',
-	// 		url: 'https://github.com/MonseGuzman/example-scripts.git'
-	// }
+	
 	dir("resources"){
 		git branch: '${SHARED_LIBRARY_VER}',
 			credentialsId: 'global',
